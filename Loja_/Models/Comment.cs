3e5ffff3d18ef65ed926108deabd2036104cb9fe
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-30 20:00:10 -0300

    Adicionar arquivos de projeto.

---
+++ Loja_/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Loja_.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public int PostId { get; set; }
+        public Produto Produto { get; set; }
+    }
+}
---
+++ Loja_/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loja_.Models
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public int OwnerID { get; set; } = 0;
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string? Resume { get; set; } = "";
+        public string Category { get; set; } = "";
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+        public string? Description { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string UserImage { get; set; } = "";
+        public bool Active { get; set; } = true;
+        
+    }
+}
+
